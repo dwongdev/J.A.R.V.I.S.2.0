92bc06d2ef34ccb7ffa5c9ab0a99fc886d9660b4
ganesh nikhil <73976037+ganeshnikhil@users.noreply.github.com>
2025-04-01 19:22:44 +0530

    Update link_op.py

--- src/FUNCTION/link_op.py
+++ src/FUNCTION/link_op.py
@@ -8,28 +8,10 @@ def search_youtube(topic:str) -> None:
     webbrowser.open(link)
     return None 
 
-def open_youtube():
-    """Open YouTube in a web browser."""
-    link = f"https://www.youtube.com"
-    webbrowser.open(link)
-    return None 
-
-
-def open_github():
-    """Open GitHub in a web browser."""
-    link = f"https://github.com"
-    webbrowser.open(link)
-    return None 
 
 def yt_trending():
     """Open Youtube trending page."""
     link = f"https://www.youtube.com/feed/trending"
-    webbrowser.open(link)
-    return None 
-
-def open_instagram():
-    """Open Instagram in a web browser."""
-    link = f"https://www.instagram.com"
     webbrowser.open(link)
     return None 
 
