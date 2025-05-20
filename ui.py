6043545e1732099343beac1a076d62337b727b46
GANESH NIKHIL <ganeshnikhil@GANESHs-Laptop.local>
2025-05-20 09:44:09 +0530

    added spinner ui

--- ui.py
+++ ui.py
@@ -212,7 +212,10 @@ if user_input := st.chat_input("Ask me anything... "):
         response = personal_chat_ai(user_input)
     elif st.session_state.chat_mode == "chat_with_rag":
         subject = st.session_state.rag_subject
-        response = chat_with_rag_session(subject, user_input) if subject else "🚨 Please enter a subject."
+        if user_input:###
+            with st.spinner("Thinking..."): #####
+                response = chat_with_rag_session(subject, user_input) if subject else "🚨 Please enter a subject."
+                
     elif st.session_state.chat_mode == "data_analysis":
         try:
             if not st.session_state.uploaded_file_path:
